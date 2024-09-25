8c89466d488a7374e24581165de0c8186227a319
hataboon <straightener0201@hotmail.co.jp>
2024-09-25 21:18:03 +0900

    デザイン修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,13 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+  theme: {
+    extend: {
+      fontSize: {
+        sm: "2rem",
+      },
+    },
+  },
+  plugins: [],
 }
